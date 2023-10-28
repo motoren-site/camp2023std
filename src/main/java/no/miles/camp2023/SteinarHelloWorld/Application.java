c1b774a6d6377ff90c9d677d0e2b1dfea6b50d53
Steinar Dragsnes <stdra@fiskeridir.no>
2023-10-28 15:06:32 +0200

    deployment.yml

--- src/main/java/no/miles/camp2023/SteinarHelloWorld/Application.java
+++ src/main/java/no/miles/camp2023/SteinarHelloWorld/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SteinarHelloWorldApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SteinarHelloWorldApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
