68210720145939df57306037a8cf1e598c472833
Steinar Dragsnes <stdra@fiskeridir.no>
2023-10-28 15:41:51 +0200

    path

--- src/main/java/no/miles/camp2023/SteinarHelloWorld/HelloController.java
+++ src/main/java/no/miles/camp2023/SteinarHelloWorld/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController
 
     private static final String PRODUCES = "application/json; charset=UTF-8";
 
-    @GetMapping(path = "/hello", produces = PRODUCES)
+    @GetMapping(path = "/camp2023std/hello", produces = PRODUCES)
     public ResponseEntity<String> hello() {
         return ResponseEntity.ok("Hi!");
     }
